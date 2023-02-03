a9fdab36227fd1a9bc5f43ad94db309a2c21af2d
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-03 20:51:59 +0100

    Create setup3.sh

---
+++ setup3.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+pacman -Syu base-devel git python-pip unzip
+paru -S git hyprland-git polkit-gnome ffmpeg swaybg polkit-kde-agent dunst grimblast rofi rofi-emoji wl-clipboard wf-recorder wlogout grimblast-git hyprpicker-git hyprpaper-git xdg-desktop-portal-hyprland-git ffmpegthumbnailer tumbler wtype colord imagemagick swaylock-effects qt5-wayland qt6-wayland ripgrep waybar-hyprland-git catppuccin-gtk-theme-mocha catppuccin-cursors-mocha catppuccin-mocha-grub-theme-git playerctl nwg-look cava pavucontrol ranger zsh starship neovim viewnior noise-suppression-for-voice thunar thunar-archive-plugin file-roller rsync wezterm
+git clone https://github.com/linuxmobile/hyprland-dots $HOME/Downloads/hyprland-dots/
+cd $HOME/Downloads/hyprland-dots/
+rsync -avxHAXP --exclude '.git*' .* ~/
+mkdir -p $HOME/Downloads/nerdfonts/
+cd $HOME/Downloads/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.1/CascadiaCode.zip
+unzip '*.zip' -d $HOME/Downloads/nerdfonts/
+rm -rf *.zip
+sudo cp -R $HOME/Downloads/nerdfonts/ /usr/share/fonts/
+fc-cache -rv
