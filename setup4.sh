4ab47ce257e2e3b62d6da2565fe3123826ae8828
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-04 00:52:03 +0100

    Update setup4.sh

--- setup4.sh
+++ setup4.sh
@@ -1,10 +1,4 @@
 !#/bin/bash
-pacman -Syu base-devel git python-pip unzip rsync bash-completion fish
-mkdir Downloads && cd Downloads/
-mkdir _cloned-repos && cd _cloned-repos
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
 paru -S hyprland-git polkit-gnome ffmpeg swaybg polkit-kde-agent dunst grimblast rofi rofi-emoji wl-clipboard wf-recorder wlogout grimblast-git hyprpicker-git hyprpaper-git xdg-desktop-portal-hyprland-git ffmpegthumbnailer tumbler wtype colord imagemagick swaylock-effects qt5-wayland qt6-wayland ripgrep waybar-hyprland-git catppuccin-gtk-theme-mocha catppuccin-cursors-mocha catppuccin-mocha-grub-theme-git playerctl nwg-look cava pavucontrol ranger zsh starship neovim viewnior noise-suppression-for-voice thunar thunar-archive-plugin file-roller wezterm pamixer wlr-randr wtype ttf-twemoji-color noto-fonts-emoji sddm
 pip install clang-tidy
 pip install dulwich
