f0f997ed0a869363b9d45c6417da120f77606ddb
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-03 20:56:28 +0100

    Update setup3.sh

--- setup3.sh
+++ setup3.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-pacman -Syu base-devel git python-pip unzip python-pip
+pacman -Syu base-devel git python-pip unzip
 paru -S hyprland-git polkit-gnome ffmpeg swaybg polkit-kde-agent dunst grimblast rofi rofi-emoji wl-clipboard wf-recorder wlogout grimblast-git hyprpicker-git hyprpaper-git xdg-desktop-portal-hyprland-git ffmpegthumbnailer tumbler wtype colord imagemagick swaylock-effects qt5-wayland qt6-wayland ripgrep waybar-hyprland-git catppuccin-gtk-theme-mocha catppuccin-cursors-mocha catppuccin-mocha-grub-theme-git playerctl nwg-look cava pavucontrol ranger zsh starship neovim viewnior noise-suppression-for-voice thunar thunar-archive-plugin file-roller rsync wezterm pamixer
 git clone https://github.com/linuxmobile/hyprland-dots $HOME/Downloads/hyprland-dots/
 pip install dulwich
