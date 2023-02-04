225dc459821bb5b5c6ba4f70fa102cb2eec83196
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-04 03:29:40 +0100

    Delete first-setup.sh

--- first-setup.sh
+++
@@ -1,7 +0,0 @@
-!#/bin/bash
-pacman -Syu base-devel git python-pip unzip rsync bash-completion fish
-mkdir Downloads && cd Downloads/
-mkdir _cloned-repos && cd _cloned-repos
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
