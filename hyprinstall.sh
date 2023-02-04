b3c78993cd1c9df7832c178a893b348cf2adbaaa
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-04 18:38:19 +0100

    Update hyprinstall.sh

--- hyprinstall.sh
+++ hyprinstall.sh
@@ -1,5 +1,6 @@
 !#/bin/bash
 paru -S hyprland-git polkit-gnome ffmpeg swaybg polkit-kde-agent dunst grimblast rofi rofi-emoji wl-clipboard wf-recorder wlogout grimblast-git hyprpicker-git hyprpaper-git xdg-desktop-portal-hyprland-git ffmpegthumbnailer tumbler wtype colord imagemagick swaylock-effects qt5-wayland qt6-wayland ripgrep waybar-hyprland-git catppuccin-gtk-theme-mocha catppuccin-cursors-mocha catppuccin-mocha-grub-theme-git playerctl nwg-look cava pavucontrol ranger zsh starship neovim viewnior noise-suppression-for-voice thunar thunar-archive-plugin file-roller wezterm pamixer wlr-randr wtype ttf-twemoji-color noto-fonts-emoji
+pip install clang-tidy dulwich requests datetime
 git clone https://github.com/raminsamadi123/hyprinstall $HOME/Downloads/hyprinstall/
 cd $HOME/Downloads/hyprinstall/
 rsync -avxHAXP --exclude '.git*' .* ~/
