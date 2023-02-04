e4024ab91fc9bb730a0993a2a08d2ffef5bb771a
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-04 22:19:51 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo pacman -Syu base-devel git python-pip unzip rsync bash-completion fish sddm
-mkdir Downloads && cd Downloads/
-mkdir _cloned-repos && cd _cloned-repos
-git clone https://aur.archlinux.org/paru.git
-cd paru/
-makepkg -si
