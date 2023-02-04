38a9a8d5756b7848fe863bce6728bcab1cea6ead
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-04 03:23:59 +0100

    Update setup4.sh

--- setup4.sh
+++ setup4.sh
@@ -10,8 +10,6 @@ rsync -avxHAXP --exclude '.git*' .* ~/
 mkdir -p $HOME/Downloads/nerdfonts/
 cd $HOME/Downloads/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.1/CascadiaCode.zip
-unzip '*.zip' -d $HOME/Downloads/nerdfonts/
-rm -rf *.zip
 wget https://github.com/raminsamadi123/hyprinstall/archive/refs/tags/Fonts.zip
 unzip '*.zip' -d $HOME/Downloads/nerdfonts/
 rm -rf *.zip
