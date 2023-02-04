46c377295c07ccf50d53d971d45837ae980d961e
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-04 03:29:49 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,1 +0,0 @@
-!#/bin/bashsudo pacman -Syu base-devel git python-pip && yay -S hyprland-git polkit-gnome ffmpeg neovim viewnior dunst rofi pavucontrol ranger zsh starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar unzip wezterm
