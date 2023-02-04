28112c17097ab2f41917b76bb76c18b25ca14194
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-04 21:31:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,3 @@ mkdir _cloned-repos && cd _cloned-repos
 git clone https://aur.archlinux.org/paru.git
 cd paru/
 makepkg -si
-cd && git clone https://github.com/raminsamadi123/hyprinstall
-cd hyprinstall/
-chmod +x hyprinstall.sh
-./hyprinstall.sh
