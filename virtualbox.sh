b6426900e0cc1b242cc8b30ea20776bed5b82c7e
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-06 23:33:48 +0100

    Update virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+echo "What are you using? (Nvidia / VirtualBox / If none of these press enter):"
+read graphics
+
+echo "install dependencies"
+
+if [[ "$graphics" =~ ^[nN][vV][iI][dD][iI][aA]$ ]]; then
+	echo "nvidia"
+elif [[ "$graphics" =~ ^[vV][iI][rR][tT][uU][aA][lL][bB][oO][xX]$ ]]; then
+    echo "virtualbox" 
+fi
+
+echo "none"
