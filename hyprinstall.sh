3ecd7cd4caea64470e7ab6667a2a770268403fa6
raminsamadi123 <112062741+raminsamadi123@users.noreply.github.com>
2023-02-14 17:25:50 +0100

    Update hyprinstall.sh

--- hyprinstall.sh
+++ hyprinstall.sh
@@ -69,7 +69,6 @@ sudo mkdir /etc/systemd/system/getty@tty1.service.d
 sudo touch override.conf /etc/systemd/system/getty@tty1.service.d/
 sudo sh -c "echo -e '[Service]\nExecStart=\nExecStart=-/sbin/agetty --noissue --autologin $USER %I \$TERM\nType=idle' > /etc/systemd/system/getty@tty1.service.d/override.conf" 
 sudo cp ~/.local/bin/wrappedhl /usr/share/wayland-sessions/wrapped_hl.desktop
-sudo chmod +x /usr/share/wayland-sessions/wrapped_hl.desktop
 echo '
 #
 # ~/.bash_profile
